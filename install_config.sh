21ca51fe394504d193e0d1f53d13c10fc23c6f8c
joundy <hafizjoundys@gmail.com>
2022-11-01 00:35:44 +0700

    fix: install_config.sh

--- install_config.sh
+++ install_config.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 echo "Creating symbolic links for dotfiles..."
-ln -s ./.tmux.conf ~/.tmux.conf
-ln -s ./.gitconfig ~/.gitconfig
-ln -s ./.zshrc ~/.zshrc
-ln -s ./sh ~/sh
-ln -s ./.config/rofi ~/.config/rofi
-ln -s ./.config/regolith2 ~/.config/regolith2
-ln -s ./.config/nvim/lua/custom ~/.config/nvim/lua/custom
+
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/sh ~/sh
+ln -s ~/dotfiles/.config/rofi ~/.config/rofi
+ln -s ~/dotfiles/.config/regolith2 ~/.config/regolith2
+ln -s ~/dotfiles/.config/nvim/lua/custom ~/.config/nvim/lua/custom
 
 echo "done"
